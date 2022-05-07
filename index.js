3a80e1ff5445ca1cf4763974ab497bee91e34406
Anusuya Bhattacharya <rupsa9875594718@gmail.com>
2022-05-07 14:06:14 +0530

    single-data-load-done

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ async function run() {
             res.send(dresses);
         })
 
-        app.get('/dresses/:id', async (req, res) => {
+        app.get('/dress/:id', async (req, res) => {
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
             const dress = await dressesCollection.findOne(query);
