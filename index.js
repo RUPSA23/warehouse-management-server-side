e1f872f04ce2dcbc5bc82dff91889860b3c388c5
Anusuya Bhattacharya <rupsa9875594718@gmail.com>
2022-05-07 13:34:22 +0530

    dataload-from-db-done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -13,12 +13,32 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uuesw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('Warehouse project DB connected');
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try{
+        await client.connect();
+        const dressesCollection = client.db('warehouse').collection('dresses');
+
+        app.get('/dresses', async (req, res) => {
+            const query = {};
+            const cursor = dressesCollection.find(query);
+            const dresses = await cursor.toArray();
+            res.send(dresses);
+        })
+
+        app.get('/dresses/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const dress = await dressesCollection.findOne(query);
+            res.send(dress);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
